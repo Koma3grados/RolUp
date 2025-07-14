41e252be39db39c1e2d9196e9742e51e42601554
Koma3grados <komasteleck@gmail.com>
2025-07-14 20:44:03 +0200

    Proyecto Base
    Estructura del proyecto inicial (para comenzar por el backend, por lo que el frontend no está aún creado)

---
+++ backend/src/main/java/com/rolup/backend/RolUpApplication.java
@@ -0,0 +1,13 @@
+package com.rolup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolUpApplication.class, args);
+	}
+
+}
